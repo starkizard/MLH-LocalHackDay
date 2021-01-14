9ea89b1f89d566593915f1f6dca64eddbacb5ebb
Prabal Kalia <42800934+starkizard@users.noreply.github.com>
2021-01-14 22:12:55 +0530

    Add files via upload

---
+++ sendgrid/README.md
@@ -0,0 +1,10 @@
+# Use-the-sendgrid-api
+MLH Local Hack Day: Build 2021 Day 4 Task: Use the Sendgrid API
+
+# Task Statement
+Have you ever used the Sendgrid email API? Use it and you’ll be ready to automate sending emails. Show off how you used it on our Day 4 Devpost!
+
+# Submission
+
+## Send an e-mail using the Sendgrid API
+A simple python script to send an email to anyone using the Twilio Sendgrid API
---
+++ sendgrid/email.py
@@ -0,0 +1,17 @@
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+message = Mail(
+    from_email='from_email@example.com',
+    to_emails='to@example.com',
+    subject='Test-Sample For MLH LHD',
+    html_content='')
+try:
+    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e.message)
